80ca984c65d9d44b005b40d1e7ad29b8067ec59a
manuel <mmiranda31@gatech.edu>
2025-02-25 13:03:48 -0700

    Added new styles and dark mode

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   // Future dynamic interactions or animations can be added here.
 });
+document.getElementById('dark-toggle').addEventListener('click', function() {
+  document.body.classList.toggle('dark-mode');
+});
